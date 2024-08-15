961f9dba0b8c2ceddd89b41cbd70c55c6b64b6b9
Sherry <shitecodegenerator@gmail.com>
2024-08-15 19:40:59 +0800

    fix

--- src/views/EnterData.vue
+++ src/views/EnterData.vue
@@ -1,158 +1,177 @@
 <template>
-    <div class="bg h-screen px-4 pt-[20vh]">
-
-<div class="bg-white shadow rounded-lg border  flex flex-col items-center justify-center py-8 px-4">
-    <h1 v-if="!done" class="font-bold text-xl text-center mb-4 text-blue-700">{{ eventName }}<br>
-      葡眾珍鑽經理贈車表揚大會<br>參加者基本資料（僅限個人參與）</h1>
-    <el-form
-     v-if="!done"
-      ref="ruleFormRef"
-      :model="ruleForm"
-      status-icon
-      :rules="rules"
-      label-width="140px"
-      label-position="left"
-      
-      class="demo-ruleForm"
+  <div class="bg h-screen px-4 pt-[20vh]">
+    <div
+      class="bg-white shadow rounded-lg border flex flex-col items-center justify-center py-8 px-4"
     >
-      <!-- <el-form-item label="場次" prop="event">
+      <h1 v-if="!done" class="font-bold text-xl text-center mb-4 text-blue-700">
+        {{ eventName }}<br />
+        葡眾珍鑽經理贈車表揚大會<br />參加者基本資料（僅限個人參與）
+      </h1>
+      <el-form
+        v-if="!done"
+        ref="ruleFormRef"
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        label-width="140px"
+        label-position="left"
+        class="demo-ruleForm"
+      >
+        <!-- <el-form-item label="場次" prop="event">
         <el-input disabled v-model="ruleForm.event"  autocomplete="off" />
       </el-form-item> -->
-      <el-form-item label="姓名" prop="name">
-        <el-input v-model="ruleForm.name"  autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="手機號碼" prop="mobile">
-        <el-input v-model="ruleForm.mobile"  autocomplete="off" />
-      </el-form-item>
-      <el-form-item label="身分證字號" prop="userId">
-        <el-input
-          v-model="ruleForm.userId"
-          autocomplete="off"
-        />
-      </el-form-item>
-    </el-form>
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="ruleForm.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="手機號碼" prop="mobile">
+          <el-input v-model="ruleForm.mobile" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="身分證字號" prop="userId">
+          <el-input v-model="ruleForm.userId" autocomplete="off" />
+        </el-form-item>
+      </el-form>
 
-    <div v-if="!done" class="flex flex-col gap-4 w-full">
-        <el-button type="primary"  style="margin: 0;height: 48px;" @click="submitForm(ruleFormRef)"
+      <div v-if="!done" class="flex flex-col gap-4 w-full">
+        <el-button
+          type="primary"
+          style="margin: 0; height: 48px"
+          @click="submitForm(ruleFormRef)"
           >送出</el-button
         >
-        <el-button style="margin: 0;height: 48px;" @click="resetForm(ruleFormRef)">重置</el-button>
+        <el-button
+          style="margin: 0; height: 48px"
+          @click="resetForm(ruleFormRef)"
+          >重置</el-button
+        >
         <p class="text-gray-500" v-html="note"></p>
+      </div>
+
+      <div
+        v-else
+        class="text-green-500 flex flex-col items-center justify-center gap-10 font-bold text-4xl"
+      >
+        <CircleCheckFilled />
+        <span>報名成功</span>
+      </div>
     </div>
+  </div>
+</template>
 
-    <div v-else class="text-green-500 flex flex-col items-center justify-center gap-10 font-bold text-4xl">
-      <CircleCheckFilled />
-      <span>報名成功</span>
-    </div>
-   </div>
-    </div>
- 
-  </template>
-  
-  <script lang="ts" setup>
-  import { onMounted, reactive, ref, computed } from 'vue';
-  import type { FormInstance, FormRules } from 'element-plus'
-  import { ElMessage, ElLoading } from 'element-plus';
-  import axios from 'axios'
-  import { CircleCheckFilled } from '@element-plus/icons-vue'
-  import { useRoute } from 'vue-router' 
-  
-  const ruleFormRef = ref<FormInstance>()
-  const done = ref(false)
+<script lang="ts" setup>
+import { onMounted, reactive, ref, computed } from "vue";
+import type { FormInstance, FormRules } from "element-plus";
+import { ElMessage, ElLoading } from "element-plus";
+import axios from "axios";
+import { CircleCheckFilled } from "@element-plus/icons-vue";
+import { useRoute } from "vue-router";
 
-  const eventName = computed(() => {
-    if(ruleForm.event === 1) {
-      return `113年3月9日台中場`
+const ruleFormRef = ref<FormInstance>();
+const done = ref(false);
+
+const eventName = computed(() => {
+  if (ruleForm.event === 1) {
+    return `113年8月24日高雄場`;
+  }
+
+  if (ruleForm.event === 2) {
+    return `113年9月21日台中場`;
+  }
+
+  if (ruleForm.event === 3) {
+    return `113年9月22日台中場`;
+  }
+  if (ruleForm.event === 4) {
+    return `113年9月28日林口場`;
+  }
+});
+
+const note = computed(() => {
+  return `備註:<br>
+得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`;
+});
+
+const ruleForm = reactive({
+  name: "",
+  userId: "",
+  mobile: "",
+  event: 1,
+});
+
+const rules = reactive<FormRules<typeof ruleForm>>({
+  name: [
+    {
+      required: true,
+      message: "請輸入姓名以驗證中獎人身份",
+      trigger: "change",
+    },
+  ],
+  mobile: [
+    {
+      required: true,
+      message: "請輸入電話以驗證中獎人身份",
+      trigger: "change",
+    },
+  ],
+  event: [{ required: true }],
+  userId: [{ required: true, message: "請輸入身分證字號", trigger: "change" }],
+});
+
+const submitForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  const loading = ElLoading.service({
+    lock: true,
+    customClass: "spinner",
+    background: "rgba(0, 0, 0, 0.7)",
+  });
+  formEl.validate(async (valid) => {
+    if (!valid) {
+      loading.close();
     }
-
-    if(ruleForm.event === 2) {
-      return `113年3月30日高雄場`
-    }
-
-    if(ruleForm.event === 3) {
-      return `113年4月13日林口場`
-    }
-  })
-  
-  const note = computed(() => {
-    if(ruleForm.event === 1) {
-      return `備註:<br>
-得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`
-    }
-
-    if(ruleForm.event === 2) {
-      return `備註:<br>
-      得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`
-    }
-
-    if(ruleForm.event === 3) {
-      return `備註:<br>
-      得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`
-    }
-  })
-  
-
-  const ruleForm = reactive({
-    name: '',
-    userId: '',
-    mobile: '',
-    event: 1
-  })
-  
-  const rules = reactive<FormRules<typeof ruleForm>>({
-    name: [{ required: true, message: '請輸入姓名以驗證中獎人身份', trigger: 'change' }],
-    mobile: [{ required: true, message: '請輸入電話以驗證中獎人身份', trigger: 'change' }],
-    event: [{ required: true}],
-    userId: [
-      { required: true , message: '請輸入身分證字號', trigger: 'change'},],
-  })
-  
-  const submitForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return
-    const loading = ElLoading.service({
-      lock: true,
-      customClass: 'spinner',
-      background: 'rgba(0, 0, 0, 0.7)',
-    })
-    formEl.validate(async (valid) => {
-      if(!valid) {
-        loading.close()
+    if (valid) {
+      try {
+        ruleForm.userId = ruleForm.userId.toUpperCase();
+        const { data } = await axios.post(
+          "https://propartnerbe.vercel.app/enroll",
+          ruleForm
+        );
+        ElMessage.success("參加成功，每位來賓僅能參加一次");
+        done.value = true;
+        loading.close();
+      } catch (err) {
+        console.log(err);
+        done.value = false;
+        ElMessage.error(err?.response?.data?.message ?? "參加失敗");
+        loading.close();
       }
-      if (valid) {
-        try {
-            ruleForm.userId = ruleForm.userId.toUpperCase()
-            const { data } = await axios.post('https://propartnerbe.vercel.app/enroll', ruleForm)
-            ElMessage.success('參加成功，每位來賓僅能參加一次')
-            done.value = true
-            loading.close()
-        } catch (err) {
-            console.log(err)
-            done.value = false
-            ElMessage.error(err?.response?.data?.message ?? '參加失敗')
-            loading.close()
-        }
-      } 
-    })
-  }
-  
-  const resetForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return
-    formEl.resetFields()
-  }
-
-  const route = useRoute()
-
-  onMounted(() => {
-    if (route.query.event) {
-        ruleForm.event = +route.query.event
     }
-  })
-  </script>
+  });
+};
 
-  <style lang="scss" scoped>
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return;
+  formEl.resetFields();
+};
+
+const route = useRoute();
+
+onMounted(() => {
+  if (route.query.event) {
+    ruleForm.event = +route.query.event;
+  }
+});
+</script>
+
+<style lang="scss" scoped>
 .bg {
-    background-color: #fdfdfd;
-opacity: 1;
-background-image: linear-gradient(to right, #fff7ce, #fff7ce 6px, #fdfdfd 6px, #fdfdfd );
-background-size: 12px 100%;
-}</style>
+  background-color: #fdfdfd;
+  opacity: 1;
+  background-image: linear-gradient(
+    to right,
+    #fff7ce,
+    #fff7ce 6px,
+    #fdfdfd 6px,
+    #fdfdfd
+  );
+  background-size: 12px 100%;
+}
+</style>
--- src/views/Winners.vue
+++ src/views/Winners.vue
@@ -1,100 +1,92 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue';
-import { ElMessage } from 'element-plus';
-import axios from 'axios';
-import { useRoute } from 'vue-router';
+import { ref, onMounted, computed } from "vue";
+import { ElMessage } from "element-plus";
+import axios from "axios";
+import { useRoute } from "vue-router";
 
-const event = ref(1)
-const raw = ref(false)
+const event = ref(1);
+const raw = ref(false);
 
-
-const winners = ref([])
+const winners = ref([]);
 
 const getWinners = async () => {
   try {
-    const { data } = await axios.get(`https://propartnerbe.vercel.app/getWinners?event=${event.value}`)
-    winners.value = data.winners
-  } catch(err) {
-    ElMessage.error(err?.response?.data?.message ??'取得失敗')
+    const { data } = await axios.get(
+      `https://propartnerbe.vercel.app/getWinners?event=${event.value}`
+    );
+    winners.value = data.winners;
+  } catch (err) {
+    ElMessage.error(err?.response?.data?.message ?? "取得失敗");
   }
-}
-
+};
 
 function maskTWID(twid) {
   if (twid.length !== 10 || !/[A-Z][0-9]{9}/.test(twid)) {
-    return '身分證字號錯誤';
+    return "身分證字號錯誤";
   }
-  
-  return twid.replace(/(?<=^[A-Z]\d{3})\d{3}(?=\d{3}$)/, '***');
+
+  return twid.replace(/(?<=^[A-Z]\d{3})\d{3}(?=\d{3}$)/, "***");
 }
 function maskName(name) {
-  console.log(name, name.length)
+  console.log(name, name.length);
   if (name.length <= 2) {
-    return name[0] + '*';
+    return name[0] + "*";
   } else {
-    return name[0] + '*' + name[name.length - 1];
+    return name[0] + "*" + name[name.length - 1];
   }
 }
 
-const route = useRoute()
-
+const route = useRoute();
 
 const eventName = computed(() => {
-    if(event.value === 1) {
-      return `113年3月9日台中場`
-    }
+  if (event.value === 1) {
+    return `113年8月24日高雄場`;
+  }
 
-    if(event.value === 2) {
-      return `113年3月30日高雄場`
-    }
+  if (event.value === 2) {
+    return `113年9月21日台中場`;
+  }
 
-    if(event.value === 3) {
-      return `113年4月13日林口場`
-    }
-  })
-  
-  const note = computed(() => {
-    if(event.value === 1) {
-      return `備註:<br>
-      得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`
-    }
+  if (event.value === 3) {
+    return `113年9月22日台中場`;
+  }
+  if (event.value === 4) {
+    return `113年9月28日林口場`;
+  }
+});
 
-    if(event.value === 2) {
-      return `備註:<br>
-      得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`
-    }
-
-    if(event.value === 3) {
-      return `備註:<br>
-      得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`
-    }
-  })
-
+const note = computed(() => {
+  return `備註:<br>
+      得獎者請於當日公告時間內，攜帶有照證件至服務台領取獎品，逾期或資料錯誤，視同放棄得獎資格。`;
+});
 
 onMounted(() => {
-  if(route.query.event) {
-    event.value = +route.query.event
-    raw.value = !!route.query.raw
-    getWinners()
+  if (route.query.event) {
+    event.value = +route.query.event;
+    raw.value = !!route.query.raw;
+    getWinners();
   }
-})
-
-
-
-
+});
 </script>
 
 <template>
-  <div class="bg min-h-screen  w-screen pt-[5vh] pb-[10vh] px-4">
-    <h1 class="font-bold mb-4 text-2xl  text-blue-700">{{ eventName }}<br>
-      葡眾珍鑽經理贈車表揚大會<br>得獎名單</h1>
-      <p class="text-gray-500 bg-white p-4 border border-gray-300 mb-4 rounded" v-html="note"></p>
+  <div class="bg min-h-screen w-screen pt-[5vh] pb-[10vh] px-4">
+    <h1 class="font-bold mb-4 text-2xl text-blue-700">
+      {{ eventName }}<br />
+      葡眾珍鑽經理贈車表揚大會<br />得獎名單
+    </h1>
+    <p
+      class="text-gray-500 bg-white p-4 border border-gray-300 mb-4 rounded"
+      v-html="note"
+    ></p>
     <div class="grid grid-cols-3 gap-4">
       <div v-for="i in winners" class="flex flex-col text-black">
-        <span class="font-bold text-xl">{{raw ? i.name :  maskName(i.name) }}</span>
+        <span class="font-bold text-xl">{{
+          raw ? i.name : maskName(i.name)
+        }}</span>
         <span>{{ raw ? i.userId : maskTWID(i.userId) }}</span>
         <span v-if="raw">{{ i.mobile }}</span>
-    </div>
+      </div>
     </div>
   </div>
 </template>
@@ -102,8 +94,9 @@ onMounted(() => {
 <style scoped lang="scss">
 .bg {
   background-color: #ffffff;
-opacity: 0.8;
-background-image:  linear-gradient(#fcf4c0 1px, transparent 1px), linear-gradient(to right, #fcf4c0 1px, #ffffff 1px);
-background-size: 20px 20px;
+  opacity: 0.8;
+  background-image: linear-gradient(#fcf4c0 1px, transparent 1px),
+    linear-gradient(to right, #fcf4c0 1px, #ffffff 1px);
+  background-size: 20px 20px;
 }
 </style>
